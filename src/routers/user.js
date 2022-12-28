88be6705ea0a8cd5d0b0ad05189ba64baf8a25b2
nicolasrobertocervantes <nicolasrobertocervantes@gmail.com>
2022-12-28 19:13:02 -0400

    Logout and logoutAll

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,6 +1,6 @@
 const express = require('express')
-const { default: mongoose } = require('mongoose')
 const User = require('../models/user')
+const auth = require('../middleware/auth')
 const router = new express.Router()
 
 router.post('/users', async (req, res) => {
@@ -15,14 +15,8 @@ router.post('/users', async (req, res) => {
     }
 })
 
-router.get('/users', async (req, res) => {
-
-    try{
-        const users = await User.find({})
-        res.send(users)
-    } catch(e) {
-        res.status(500).send(e)
-    }
+router.get('/users/me', auth, async (req, res) => {
+    res.send(req.user)
 })
 
 router.get('/users/:id', async (req, res) => {
@@ -89,6 +83,29 @@ router.post('/users/login', async (req, res) => {
         res.send({ user, token })
     } catch(e) {
         res.status(400).send()
+    }
+})
+
+router.post('/users/logout', auth,async (req, res) => {
+    try {
+        req.user.tokens = req.user.tokens.filter((token) => {
+            return token.token !== req.token
+        })
+        await req.user.save()
+
+        res.send()
+    } catch (e) {
+        res.status(500).send()
+    }
+})
+
+router.post('/users/logoutAll', auth, async(req, res) => {
+    try {
+        req.user.tokens = []
+        await req.user.save()
+        res.send()
+    } catch (e) {
+        res.status(500).send()
     }
 })
 
